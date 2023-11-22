b0e29a0678ec94e1af5ae84408ea1a8938d71c86
Kenneth <alcanamay@gmail.com>
2023-11-22 04:27:15 -0600

    Prefab Enemy

---
+++ NewBackyard/Assets/Scripts/IA/Controllers/EnemyController2.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController2 : MonoBehaviour
+{
+    
+    private BehaviourController _behaviourController;
+    private WanderEnemy _wanderEnemy;
+    private Seek _seek;
+    private AvoidCollision _avoidCollision;
+    private PathFollowing _pathFollowing;
+    public GameObject target;
+
+    public bool canSend;
+
+    public enum State
+    {
+        Idle,
+        Follow,
+        Zone1,
+        Zone2,
+        Zone3,
+        Zone4,
+        ZoneCenter,
+    }
+    public State enemyState;
+
+    private void Start()
+    {
+        target = GameObject.Find("Capsule");
+        _behaviourController = GetComponent<BehaviourController>();
+        _wanderEnemy = GetComponent<WanderEnemy>();
+        _seek = GetComponent<Seek>();
+        _avoidCollision = GetComponent<AvoidCollision>();
+        _pathFollowing = GetComponent<PathFollowing>();
+        canSend = false;
+    }
+
+    private void Update()
+    {
+        switch (enemyState)
+        {
+            case State.Idle:
+                _behaviourController.behaviours[0] = _wanderEnemy;
+                _wanderEnemy.enabled = true;
+                _pathFollowing.enabled = false;
+                break;
+            case State.Follow:
+                _behaviourController.behaviours[0] = _seek;
+                _pathFollowing.enabled = false;
+                _wanderEnemy.enabled = false;
+                _seek.Target = target.transform.position;
+                canSend = true;
+                break;
+            case State.Zone1:
+                _behaviourController.behaviours[0] = _pathFollowing;
+                _pathFollowing.endList = _pathFollowing.nodes;
+                _pathFollowing.enabled = true;
+                _wanderEnemy.enabled = false;
+                break;
+            case State.Zone2:
+                _behaviourController.behaviours[0] = _pathFollowing;
+                _pathFollowing.endList = _pathFollowing.nodes2;
+                _pathFollowing.enabled = true;
+                _wanderEnemy.enabled = false;
+                break;
+            case State.Zone3:
+                _behaviourController.behaviours[0] = _pathFollowing;
+                _pathFollowing.endList = _pathFollowing.nodes3;
+                _pathFollowing.enabled = true;
+                _wanderEnemy.enabled = false;
+                break;
+            case State.Zone4:
+                _behaviourController.behaviours[0] = _pathFollowing;
+                _pathFollowing.endList = _pathFollowing.nodes4;
+                _pathFollowing.enabled = true;
+                _wanderEnemy.enabled = false;
+                break;
+            case State.ZoneCenter:
+                _behaviourController.behaviours[0] = _pathFollowing;
+                _pathFollowing.endList = _pathFollowing.nodescenter;
+                _pathFollowing.enabled = true;
+                _wanderEnemy.enabled = false;
+                break;
+                
+        }
+
+    }
+}
