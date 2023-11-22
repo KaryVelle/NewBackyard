8edd87d89b286d5c24ac0cb5d1ac8326f52179bb
Carlos R <crono_zs@hotmail.com>
2023-11-21 22:11:41 -0600

    agregando en new input sistem y Dejando listo el Movimiento

---
+++ NewBackyard/Assets/Scripts/MoveInputs.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MoveInputs : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float rotSpeed;
+    void Update()
+    {
+        var gamepad = Gamepad.current;
+        Vector2 moveInput = gamepad.leftStick.ReadValue();
+        Vector2 cameraRot = gamepad.rightStick.ReadValue();
+
+        transform.Translate(new Vector3(moveInput.x, 0, moveInput.y) * speed * Time.deltaTime);
+        transform.Rotate(Vector3.up * cameraRot.x * rotSpeed * Time.deltaTime);
+    }
+}
