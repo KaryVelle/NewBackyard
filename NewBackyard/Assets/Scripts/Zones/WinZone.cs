02d014c1fea4d39f021b7eaa64b7779d752cba49
KaryVelle <105183305+KaryVelle@users.noreply.github.com>
2023-11-22 04:46:04 -0600

    ROTO

--- NewBackyard/Assets/Scripts/Zones/WinZone.cs
+++ NewBackyard/Assets/Scripts/Zones/WinZone.cs
@@ -2,9 +2,41 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
 
 public class WinZone : AbstracZone
 {
+   public float time1;
+   public float time2;
+   public GameObject winPlatform;
+
+      void Start()
+      {
+         StartCoroutine(WinZoneUnlocked());
+      }
+
+      IEnumerator WinZoneUnlocked()
+      {
+         yield return new WaitForSeconds(Random.Range(time1, time2));
+
+         Vector3 targetPosition = new Vector3(-36.1f, -7.1f, -80.3f);
+         float duration = 2.0f; 
+
+         float elapsedTime = 0f;
+         Vector3 initialPosition = winPlatform.transform.position;
+
+         while (elapsedTime < duration)
+         {
+            winPlatform.transform.position = Vector3.Lerp(initialPosition, targetPosition, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+         }
+         
+         winPlatform.transform.position = targetPosition;
+
+      }
+      
    private void OnTriggerEnter(Collider other)
    {
       if (other.gameObject.CompareTag("Enemy"))
@@ -47,5 +79,7 @@ public class WinZone : AbstracZone
          // los enemigos ganaron, entonces pierdes
       }
    }
+
+ 
    
 }
