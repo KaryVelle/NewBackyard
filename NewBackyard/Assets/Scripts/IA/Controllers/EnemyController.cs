6a9681db747591d1abb530bf26389324d43a2c45
Kenneth <alcanamay@gmail.com>
2023-11-21 23:44:17 -0600

    Path Following primer zona

--- NewBackyard/Assets/Scripts/IA/Controllers/EnemyController.cs
+++ NewBackyard/Assets/Scripts/IA/Controllers/EnemyController.cs
@@ -38,20 +38,25 @@ public class EnemyController : MonoBehaviour
             case State.Idle:
                 _behaviourController.behaviours[0] = _wander;
                 _wander.enabled = true;
+                _pathFollowing.enabled = false;
                 break;
             case State.Follow:
                 _behaviourController.behaviours[0] = _seek;
                 _avoidCollision.enabled = false;
+                _pathFollowing.enabled = false;
                 _wander.enabled = false;
                 _seek.Target = target.transform.position;
                 break;
             case State.Zone1:
                 _behaviourController.behaviours[0] = _pathFollowing;
+                _pathFollowing.endList = _pathFollowing.nodes;
+                _pathFollowing.enabled = true;
                 _wander.enabled = false;
                 break;
             case State.Zone2:
                 _behaviourController.behaviours[0] = _pathFollowing;
-                _pathFollowing.endList = _pathFollowing.nodes;
+                _pathFollowing.endList = _pathFollowing.nodes2;
+                _pathFollowing.enabled = true;
                 _wander.enabled = false;
                 break;
         }
