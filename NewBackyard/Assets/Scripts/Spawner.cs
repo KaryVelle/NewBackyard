aad26e59b20d4f01435d2c97636150aa2084e7b7
K-enneth <alcanamay@gmail.com>
2023-11-21 18:05:58 -0600

    Añadiendo Spawner script

---
+++ NewBackyard/Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public int maxSpawn;
+    public List<GameObject> enemyList;
+    public int spawnTime;
+
+    private void Start()
+    {
+        for (int i = 0; i <= maxSpawn; i++)
+        {
+            Vector3 randomPos = new Vector3(Random.Range(-555f, 800f), -190f, Random.Range(-700f, 500f));
+            GameObject NewInst = Instantiate(enemy, randomPos, Quaternion.identity);
+            enemyList.Add(NewInst);
+        }
+        InvokeRepeating("Spawn", 2f, spawnTime);
+    }
+
+    private void Spawn()
+    {
+        Vector3 randomPos = new Vector3(Random.Range(-555f, 800f), -190f, Random.Range(-700f, 500f));
+        GameObject newEnemy = Instantiate(enemy, randomPos, Quaternion.identity);
+    }
+}
