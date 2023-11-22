d4f994868d82d09f5adaf8ed7943440d30217d56
Carlos R <crono_zs@hotmail.com>
2023-11-22 01:08:01 -0600

    dejando listo el movimiento y preparando para hacer los cambios de estado

--- NewBackyard/Assets/Scripts/MoveInputs.cs
+++ NewBackyard/Assets/Scripts/MoveInputs.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEditor.UI;
 
 public class MoveInputs : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] float rotSpeed;
+    [SerializeField] Canvas selector;
+
     void Update()
     {
         var gamepad = Gamepad.current;
         Vector2 moveInput = gamepad.leftStick.ReadValue();
         Vector2 cameraRot = gamepad.rightStick.ReadValue();
-
+        
         transform.Translate(new Vector3(moveInput.x, 0, moveInput.y) * speed * Time.deltaTime);
         transform.Rotate(Vector3.up * cameraRot.x * rotSpeed * Time.deltaTime);
+
     }
 }
---
+++ NewBackyard/Assets/Scripts/SelectionRing.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[CreateAssetMenuAttribute(fileName = "SelectionRing", menuName = "RingMenu/State", order = 2)]
+public class SelectionRing : ScriptableObject
+{   
+    public string text;
+}
