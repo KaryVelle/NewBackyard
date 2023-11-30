ae00bb3aa4198014fad1297311512b15a6824baf
cronozs <61797440+cronozs@users.noreply.github.com>
2023-11-30 15:54:31 -0600

    arreglando un poco la conquista de zonas

--- NewBackyard/Assets/ZoneController.cs
+++ NewBackyard/Assets/ZoneController.cs
@@ -11,45 +11,67 @@ public class ZoneController : MonoBehaviour
     public FactoryZone factoryZone;
     public List<bool> zoneList;
     public List<bool> zoneListEnem;
+    public bool pSpeedZone = true;
+    public bool eSpeedZone = true;
+    public bool pBirthZone = true;
+    public bool eBirthZone = true;
+    public bool pFactoryZone = true;
+    public bool eFactoryZone = true;
+    public bool pHealthZone = true;
+    public bool eHealthZone = true;
 
     private void Update()
     {
-        if (speedZone.allyZoneConquered)
+        if (speedZone.allyZoneConquered && pSpeedZone)
         {
             zoneList.Add(speedZone);
+            pSpeedZone = false;
+            eSpeedZone = true;
         }
-        if (!speedZone.allyZoneConquered)
+        if (!speedZone.allyZoneConquered && eSpeedZone)
         {
             zoneListEnem.Add(speedZone);
+            eSpeedZone = false;
+            pSpeedZone = true;
         }
         
-        
-        if (birthZone.allyZoneConquered)
+        if (birthZone.allyZoneConquered && pBirthZone)
         {
             zoneList.Add(birthZone);
+            pBirthZone = false;
+            eBirthZone = true;
         }
-        if (!birthZone.allyZoneConquered)
+        if (!birthZone.allyZoneConquered && eBirthZone)
         {
             zoneListEnem.Add(birthZone);
+            eBirthZone = false;
+            pBirthZone = true;
         }
-        
        
-        if (factoryZone.allyZoneConquered)
+        if (factoryZone.allyZoneConquered && pFactoryZone)
         {
             zoneList.Add(factoryZone);
+            pFactoryZone = false;
+            eFactoryZone = true;
         }
-        if (!factoryZone.allyZoneConquered)
+        if (!factoryZone.allyZoneConquered && eFactoryZone)
         {
             zoneListEnem.Add(factoryZone);
+            eFactoryZone = false;
+            pFactoryZone = true;
         }
         
-        if (healthZone.allyZoneConquered)
+        if (healthZone.allyZoneConquered && pHealthZone)
         {
             zoneList.Add(healthZone);
+            pHealthZone = false;
+            eHealthZone = true;
         }
-        if (!healthZone.allyZoneConquered)
+        if (!healthZone.allyZoneConquered && eHealthZone)
         {
             zoneListEnem.Add(healthZone);
+            eHealthZone = false;
+            pHealthZone = true;
         }
     }
 }
